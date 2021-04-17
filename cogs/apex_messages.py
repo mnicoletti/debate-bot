16440a8210b1fe1a984c80bdf929ab69aa52678e
Maximiliano Nicoletti <maxin@pagotic.com>
2021-04-17 01:58:53 -0300

    closes #29: Correccion de formato a tooltip

--- cogs/apex_messages.py
+++ cogs/apex_messages.py
@@ -21,37 +21,34 @@ class ApexMessages(commands.Cog):
     @commands.command(aliases=["h"])
     @commands.bot_has_permissions(manage_messages=True, add_reactions=True)
     async def help(self, ctx):
-        embed = discord.Embed(
-            title="Debate Bot - Ayuda",
-            description="Podés revisar [codigo fuente y documentacion](https://github.com/mnicoletti/debate-bot)\n"
-            "Si querés darme una colaboración, vení y chupame un huevo. O mandá plata.",
-            color=self.color
-            )
-        embed.set_thumbnail(url=url_data.URLData.PREDATOR_LOGO)
-        embed.add_field(name='**Comandos Activos**',
-        value="""**`<apex!help | apex!h>`** - Esta pantalla de ayuda.\n
-        **`<apex!rotation | apex!maps>`** - Muestra el mapa actual en juego, su duración y el próximo mapa en rotación.\n
-        \n""",
-        inline=False)
-        embed.add_field(name='**Mapas (No implementado)**',
-        value="""**`<apex!rotation | apex!maps> pois`** - Muestra un listado de PoIs para el mapa en curso.
-        **`<apex!rotation | apex!maps> <cantidad>`** - Muestra el mapa en curso y una <cantidad> de rotaciones futuras.
-        \n""",
-        inline=False)
-        embed.add_field(name='**Perfiles**',
-        value="""**`<apex!perfil | apex!profile>`** - Muestra el perfil registrado de usuario.\n
-        **`<apex!perfil | apex!profile> save [Origin ID]`** - Crea un vinculo entre tu cuenta de Discord y tu perfil de Apex.\n
-        **`<apex!perfil | apex!profile> display`** - Muestra el perfil vinculado a esta cuenta de Discord.\n
-        **`<apex!perfil | apex!profile> ranked`** - Muestra el rango actual y puntos acumulados para el split en juego.\n
-        **`<apex!perfil | apex!profile> lastmatch`** - Resultados de la última partida.\n
-        \n""",
-        inline=False)
-        embed.add_field(name="**Challenges**",
-        value="""**`apex!challenge poi`** - Elige un PoI al azar para el mapa en curso.\n
-        **`apex!challenge legend`** - Selecciona una leyenda al azar para la próxima partida.\n
-        **`apex!challenge team`** - Armar una comp aleatoria para la próxima partida.\n""",
-        inline=False)
-        embed.set_footer(text="Hecho gratis hasta que Perfect se ponga a streamear y nos llenemos de plata.")
+        embed = discord.Embed(title="Ayuda de comandos desactivados", colour=discord.Colour(0x8f34), url="https://github.com/mnicoletti/debate-bot", description="Podés revisar [codigo fuente y documentacion](https://github.com/mnicoletti/debate-bot)\nSi querés darme una colaboración, vení y chupame un huevo. O mandá plata.")
+
+        embed.set_thumbnail(url="http://debatebot.freecluster.eu/apexdata/BotIcons/TotoLogo.png")
+        embed.set_author(name="Debate Bot", icon_url="http://debatebot.freecluster.eu/apexdata/BotIcons/TotoLogo.png")
+        embed.set_footer(text="Hecho gratis hasta que Perfect se ponga a streamear y nos llenemos de plata.", 
+        icon_url="http://debatebot.freecluster.eu/apexdata/GameIcons/apexpredator.jpeg")
+
+        embed.add_field(name="**Comandos Activos**", 
+        value="""**`<apex!help | apex!h>`**: Esta pantalla de ayuda.\n\n
+        **`<apex!rotation | apex!maps>`**: Muestra el mapa actual en juego, su duración y el próximo mapa en rotación.\n\n
+        """)
+        embed.add_field(name="**Mapas (No implementado)**", 
+        value="""**`<apex!rotation | apex!maps> pois`**: Muestra un listado de PoIs para el mapa en curso.\n\n
+        **`<apex!rotation | apex!maps> <cantidad>`**: Muestra el mapa en curso y una <cantidad> de rotaciones futuras.\n
+        """)
+        embed.add_field(name="**Perfiles**", 
+        value="""**`<apex!perfil | apex!profile>`**: Muestra el perfil registrado de usuario.\n\n
+        **`<apex!perfil | apex!profile> save [Origin ID]`**: Crea un vinculo entre tu cuenta de Discord y tu perfil de Apex.\n\n
+        **`<apex!perfil | apex!profile> display`**: Muestra el perfil vinculado a esta cuenta de Discord.\n\n
+        **`<apex!perfil | apex!profile> ranked`**: Muestra el rango actual y puntos acumulados para el split en juego.\n\n
+        **`<apex!perfil | apex!profile> lastmatch`**: Resultados de la última partida.\n\n
+        """)
+        embed.add_field(name="**Challenges**", 
+        value="""**`apex!challenge poi`**: Elige un PoI al azar para el mapa en curso.\n\n
+        **`apex!challenge legend`**: Selecciona una leyenda al azar para la próxima partida.\n\n
+        **`apex!challenge team`**: Armar una comp aleatoria para la próxima partida.\n
+        """)
+
         await ctx.send(embed=embed)
 
 def setup(bot):
