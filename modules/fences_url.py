6fa198b81af5d4f2a14aa0efe1fc1013384e8557
Maximiliano Nicoletti <maxin@pagotic.com>
2021-04-17 21:01:13 -0300

    fixes #57 and #58: datos de Fences

--- modules/fences_url.py
+++ modules/fences_url.py
@@ -4,4 +4,4 @@ from enums import url_data
 log = logging.getLogger(__name__)
 
 def obtain_fence_url(fence_type):
-    return "{0}/GameIcons/{1}Fence.png".format(url_data.BASE_URL, fence_type)
+    return "{0}/GameIcons/{1}Fence.png".format(url_data.URLData.BASE_URL, fence_type)
