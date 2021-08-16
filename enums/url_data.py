fc782e5481154abbb6fc031f725c2f2c623efaa8
Maximiliano Nicoletti <maxin@pagotic.com>
2021-08-16 10:51:24 -0300

    starts #70: cleanup y adecuacion de librerias

--- enums/url_data.py
+++ enums/url_data.py
@@ -1,5 +1,4 @@
 from enum import Enum
-import random
 
 class URLData(Enum):
     SEASON = "09"
--- sql/patches/2.2.0/02-map-data.sql
+++ sql/patches/2.2.0/02-map-data.sql
@@ -1,1 +1,11 @@
+SET @map_name = "Olympus";
 INSERT INTO apex_pois (id_maps,name,enabled) VALUES ((SELECT id FROM apex_maps WHERE name = @map_name),"The Icarus", 1);
+
+SET @map_name = "World's Edge";
+INSERT INTO apex_pois (id_maps,name,enabled) VALUES ((SELECT id FROM apex_maps WHERE name = @map_name),"Climatizer", 1);
+INSERT INTO apex_pois (id_maps,name,enabled) VALUES ((SELECT id FROM apex_maps WHERE name = @map_name),"Lava Pool", 1);
+INSERT INTO apex_pois (id_maps,name,enabled) VALUES ((SELECT id FROM apex_maps WHERE name = @map_name),"Landslide", 1);
+
+UPDATE apex_pois SET enabled = 0 WHERE name = "Train Yard";
+UPDATE apex_pois SET enabled = 0 WHERE name = "Storting Factory";
+UPDATE apex_pois SET enabled = 0 WHERE name = "Refinery";
